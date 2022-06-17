b85353f2650912f979345ac00f1f826126225eaa
Asadullah246 <asadm2258@gmail.com>
2022-06-17 08:41:36 +0600

    edited

--- Index.js
+++ Index.js
@@ -10,11 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sciop.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 async function data(){
    
     try{
